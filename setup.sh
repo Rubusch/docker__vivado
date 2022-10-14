85e3a402a7e4b1cab44db3e0da19318a475eb282
Lothar Rubusch <l.rubusch@gmail.com>
2022-10-14 18:10:06 +0100

    setup: fixes and fail early policy
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- setup.sh
+++ setup.sh
@@ -19,6 +19,7 @@ build()
 	fi
 	cd -
 }
+
 TOPDIR="$(pwd)"
 test -z "${DOCKERDIR}" && DOCKERDIR="docker"
 test -z "${DOWNLOADDIR}" && DOWNLOADDIR="download"
@@ -26,45 +27,46 @@ BASE_IMAGE="$(grep "ARG DOCKER_BASE=" -r "${DOCKERDIR}/build_context/Dockerfile"
 BASE_IMAGE_TAG="$(grep "ARG DOCKER_BASE_TAG" -r "${DOCKERDIR}/build_context/Dockerfile" | awk -F= '{ print $2 }' | tr -d '"')"
 IMAGE="$( grep "^FROM" -HIrn ${DOCKERDIR}/build_context/Dockerfile | awk '{ print $NF }' )"
 VERSION="$( echo ${IMAGE} | awk -F'-' '{print $NF}' )"
-
-## NB: check for particular user, when different user prefixed images are around
 CONTAINER="$(docker images | grep "${BASE_IMAGE}" | grep "${BASE_IMAGE_TAG}" | awk '{print $3}')"
+## NB: check for particular user, when different user prefixed images are around
 
-## check: is base container already around?
+## checks
 if [ -z "${CONTAINER}" ]; then
-	## base container is NOT around
-
-	## check: is installer provided?
-	BASEIMAGE_INSTALLER="$( ls ${DOWNLOADDIR}/petalinux-v${VERSION}-*-installer.run )"
-	if [ -z "${BASEIMAGE_INSTALLER}" ]; then
-		die "No petalinux installer provided! Please, put a petalinux-v${VERSION}-*-installer.run file in ${DOWNLOADDIR}"
-	fi
-
-	## build base image
-	git clone "https://github.com/Rubusch/docker__petalinux.git" "${BASE_IMAGE}"
-	cd "${BASE_IMAGE}"
-	git checkout "${BASE_IMAGE_TAG}"
-	mv "${TOPDIR}/${BASEIMAGE_INSTALLER}" "./docker/build_context/"
-	./setup.sh
-	cd -
+    ## base not around, build
+	DO_BUILDBASE=1
 fi
-
-## build overlay container
 CONTAINER="$( docker images | grep "${IMAGE}" | awk '{print $3}' )"
 if [ -z "${CONTAINER}" ]; then
 	## container is not around, build
-	mv "${DOWNLOADDIR}"/.env "${DOCKERDIR}"/ || die "No .env file provided"
-	mv "${DOWNLOADDIR}/Xilinx_Unified_${VERSION}_*_Lin64.bin" "${DOCKERDIR}/build_context" || die "No Xilinx_Unified_${VERSION}_*_Lin64.bin file provided"
-	build "${DOCKERDIR}" "${DRYRUN}"
-	cd "${DOCKERDIR}"
-	echo "!!! Docker finished, overwriting ${DOCKERDIR}/.env file with default user, in case adjust manually !!!"
-	echo "UID=$(id -u)" > .env
-	echo "GID=$(id -g)" >> .env
+	DO_BUILD=1
 else
-	## container around, start up
+	## container around, start
 	cd "${DOCKERDIR}"
 	xhost +
 	docker-compose -f ./docker-compose.yml run --rm "${IMAGE}" /bin/bash
+
+	## exit success
 	xhost -
+	exit 0
 fi
+
+## build
+if [ -n "${DO_BUILDBASE}" ]; then
+	git clone "https://github.com/Rubusch/docker__petalinux.git" "${BASE_IMAGE}" || die "Could not clone petalinux repo"
+	cd "${BASE_IMAGE}"
+	git checkout "${BASE_IMAGE_TAG}"
+	mv ${TOPDIR}/${DOWNLOADDIR}/petalinux-v${VERSION}-*-installer.run "./docker/build_context/" || die "No petalinux installer provided! Please, put a petalinux-v${VERSION}-*-installer.run file in ${DOWNLOADDIR}"
+	./setup.sh
+	cd "${TOPDIR}"
+fi
+if [ -n "${DO_BUILD}" ]; then
+	mv "${TOPDIR}/${DOWNLOADDIR}"/.env "${TOPDIR}/${DOCKERDIR}"/ || die "No .env file provided"
+	mv ${TOPDIR}/${DOWNLOADDIR}/Xilinx_Unified_${VERSION}_*_Lin64.bin "${TOPDIR}/${DOCKERDIR}/build_context" || die "No Xilinx_Unified_${VERSION}_*_Lin64.bin file provided"
+	build "${DOCKERDIR}" "${DRYRUN}"
+	cd "${TOPDIR}/${DOCKERDIR}"
+	echo "!!! Docker finished, overwriting ${DOCKERDIR}/.env file with default user, in case adjust manually !!!"
+	echo "UID=$(id -u)" > .env
+	echo "GID=$(id -g)" >> .env
+fi
+
 echo "READY."
