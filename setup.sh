f4c5167502adddfa89ba3133d8503297ea437053
Lothar Rubusch <l.rubusch@gmail.com>
2025-05-02 16:28:54 +0200

    setup: refac setup
    - update the comment on not provided xilinx mail / xilinx passwd
    - refac build/run logic
    
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- setup.sh
+++ setup.sh
@@ -26,50 +26,12 @@ VERSION="$( echo ${IMAGE} | awk -F'-' '{ if ($NF == "nightly") {print $(NF-1)} e
 CONTAINER="$( docker images -q ${IMAGE} 2> /dev/null )" || true
 if [ -z "${CONTAINER}" ]; then
 	## container is not around, build
-	DO_BUILD=1
-else
-	## container around, start
-	cd "${DOCKERDIR}"
-        APP="/bin/bash"
-        if [ ! -e .env ]; then
-            APP=""
-	    echo "UID=$(id -u)" > .env
-	    echo "GID=$(id -g)" >> .env
-
-            echo
-            echo "Preparing docker images - please re-run this script to enter the container image!"
-        fi
-
-	docker run \
-	       --rm \
-               --net host \
-               --name ${IMAGE} \
-               -u ${UID}:${GID} \
-               -it \
-               --privileged \
-               -e USER \
-               -e DISPLAY=$DISPLAY \
-               --env-file .env \
-               --group-add 20 \
-               --mount type=bind,source=./build_configs,target=/home/$USER/configs \
-               -v /tmp/.X11-unix:/tmp/.X11-unix \
-               -v ~/.Xauthority:/home/${USER}/.Xauthority:ro \
-               -v ~/.gitconfig:/home/${USER}/.gitconfig:ro \
-               -v ~/.ssh:/home/${USER}/.ssh \
-               -v ./workspace:/home/${USER}/workspace \
-               ${CONTAINER} \
-               ${APP}
-
-	exit 0
-fi
-
-if [ -n "${DO_BUILD}" ]; then
 	DATE="$(date +%Y%m%d%H%M)"
 	if [ -z "${XILINXMAIL}" ]; then
-		die "env variable XILINXMAIL is not set"
+		die "pls, provide an env variable XILINXMAIL (login email for xilinx)"
 	fi
 	if [ -z "${XILINXLOGIN}" ]; then
-		die "env variable XILINXLOGIN is not set"
+		die "pls, provide an env variable XILINXLOGIN (password for the xilinx login, under with the email '$XILINXLOGIN')"
 	fi
 
 	test -f ${TOPDIR}/${DOWNLOADDIR}/petalinux-v${VERSION}-*-installer.run || die "No petalinux installer provided! Please, put a petalinux-v${VERSION}-*-installer.run  in '${TOPDIR}/${DOWNLOADDIR}'"
@@ -78,16 +40,50 @@ if [ -n "${DO_BUILD}" ]; then
 	mv ${TOPDIR}/${DOWNLOADDIR}/petalinux-v${VERSION}-*-installer.run "${TOPDIR}/${DOCKERDIR}/build_context/"
 	mv ${TOPDIR}/${DOWNLOADDIR}/Xilinx_Unified_${VERSION}_*_Lin64.bin "${TOPDIR}/${DOCKERDIR}/build_context/"
 
-        cd "$DOCKERDIR"
+	cd "$DOCKERDIR"
 	docker build \
-               --tag ${IMAGE}:${DATE} \
-               --build-arg UID=${UID} \
-               --build-arg GID=${GID} \
-               --build-arg USER=${USER} \
-               --build-arg XILINXMAIL=${XILINXMAIL} \
-               --build-arg XILINXLOGIN=${XILINXLOGIN} \
-               ./build_context
+		--tag ${IMAGE}:${DATE} \
+		--build-arg UID=${UID} \
+		--build-arg GID=${GID} \
+		--build-arg USER=${USER} \
+		--build-arg XILINXMAIL=${XILINXMAIL} \
+		--build-arg XILINXLOGIN=${XILINXLOGIN} \
+		./build_context
 	cd "${TOPDIR}/${DOCKERDIR}"
+
+else
+	## container around, start
+	cd "${DOCKERDIR}"
+	APP="/bin/bash"
+	if [ ! -e .env ]; then
+		APP=""
+		echo "UID=$(id -u)" > .env
+		echo "GID=$(id -g)" >> .env
+		echo
+		echo "Preparing docker images - please re-run this script to enter the container image!"
+	fi
+
+	docker run \
+		--rm \
+		--net host \
+		--name ${IMAGE} \
+		-u ${UID}:${GID} \
+		-it \
+		--privileged \
+		-e USER \
+		-e DISPLAY=$DISPLAY \
+		--env-file .env \
+		--group-add 20 \
+		--mount type=bind,source=./build_configs,target=/home/$USER/configs \
+		-v /tmp/.X11-unix:/tmp/.X11-unix \
+		-v ~/.Xauthority:/home/${USER}/.Xauthority:ro \
+		-v ~/.gitconfig:/home/${USER}/.gitconfig:ro \
+		-v ~/.ssh:/home/${USER}/.ssh \
+		-v ./workspace:/home/${USER}/workspace \
+		${CONTAINER} \
+		${APP}
+
+	exit 0
 fi
 
 echo "READY."
