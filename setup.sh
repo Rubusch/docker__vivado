0be5016eddd09d9576a74a4f8ada8b6187773c83
Lothar Rubusch <l.rubusch@gmail.com>
2026-05-18 21:18:53 +0200

    setup: set executable permissions
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- setup.sh
+++ setup.sh
@@ -68,7 +68,9 @@ if [ -n "${DO_BUILD}" ]; then
 	test -f ${TOPDIR}/${DOWNLOADDIR}/petalinux-v${VERSION}-*-installer.run || die "No petalinux installer provided! Please, put a petalinux-v${VERSION}-*-installer.run  in '${TOPDIR}/${DOWNLOADDIR}'"
 	test -f ${TOPDIR}/${DOWNLOADDIR}/Xilinx_Unified_${VERSION}_*_Lin64.bin || die "No Xilinx_Unified_${VERSION}_*_Lin64.bin file provided in '${TOPDIR}/${DOWNLOADDIR}'"
 
+	chmod a+x ${TOPDIR}/${DOWNLOADDIR}/petalinux-v${VERSION}-*-installer.run
 	mv ${TOPDIR}/${DOWNLOADDIR}/petalinux-v${VERSION}-*-installer.run "${TOPDIR}/${DOCKERDIR}/build_context/"
+	chmod a+x ${TOPDIR}/${DOWNLOADDIR}/Xilinx_Unified_${VERSION}_*_Lin64.bin
 	mv ${TOPDIR}/${DOWNLOADDIR}/Xilinx_Unified_${VERSION}_*_Lin64.bin "${TOPDIR}/${DOCKERDIR}/build_context/"
 
         cd "$DOCKERDIR"
