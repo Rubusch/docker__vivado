0aad29bf5b8de3f408ad56831153ae80f8cf6c9e
Lothar Rubusch <l.rubusch@gmail.com>
2026-09-03 15:49:12 +0200

    refac: modernize setup
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -e
-DRYRUN="${1}"
-#UID="$(id -u)"
-GID="$(id -g)"
+DOXXXER_UID="$(id -u)"
+DOXXXER_GID="$(id -g)"
 USER="$(whoami)"
 
 die()
@@ -12,7 +11,6 @@ die()
 
 ## MAIN
 if [ -d ".git" ]; then
-	## if this is a git repo, check if we're on branch "main", then abort
 	if [ "main" = "$( git rev-parse --abbrev-ref HEAD )" ]; then
 		die "THIS IS MAIN, PLEASE CHANGE TO ONE OF THE GIT BRANCHES"
 	fi
@@ -21,65 +19,72 @@ fi
 test -z "${DOCKERDIR}" && DOCKERDIR="docker"
 test -z "${DOWNLOADDIR}" && DOWNLOADDIR="download"
 TOPDIR="$(pwd)"
-IMAGE="$( grep "^FROM" -HIrn ${DOCKERDIR}/build_context/Dockerfile | awk '{ print $NF }' )"
-VERSION="$( echo ${IMAGE} | awk -F'-' '{ if ($NF == "nightly") {print $(NF-1)} else {print $NF} }' )"
+DOCKERFILE="${TOPDIR}/${DOCKERDIR}/Dockerfile"
+
+if [ ! -f "${DOCKERFILE}" ]; then
+	die "Dockerfile not found at ${DOCKERFILE}"
+fi
+
+VERSION="$(grep -m 1 "^ARG XILINXVERSION=" "${DOCKERFILE}" | cut -d'"' -f2)"
+
+if [ -z "${VERSION}" ]; then
+	die "Could not extract XILINXVERSION from ${DOCKERFILE}"
+fi
+
+IMAGE="vivado-${VERSION}"
 CONTAINER="$( docker images -q ${IMAGE} 2> /dev/null )" || true
+
 if [ -z "${CONTAINER}" ]; then
-	## container is not around, build
-	DATE="$(date +%Y%m%d%H%M)"
-	if [ -z "${XILINXMAIL}" ]; then
-		die "pls, provide an env variable XILINXMAIL (login email for xilinx)"
-	fi
-	if [ -z "${XILINXLOGIN}" ]; then
-		die "pls, provide an env variable XILINXLOGIN (password for the xilinx login, under with the email '$XILINXLOGIN')"
+	if [ -z "${XILINXMAIL}" ] || [ -z "${XILINXLOGIN}" ]; then
+		die "Please provide XILINXMAIL and XILINXLOGIN environment variables."
 	fi
 
-	test -f ${TOPDIR}/${DOWNLOADDIR}/*_Unified_${VERSION}_*_Lin64.bin || die "No *_Unified_${VERSION}_*_Lin64.bin file provided in '${TOPDIR}/${DOWNLOADDIR}'"
-
-	chmod a+x ${TOPDIR}/${DOWNLOADDIR}/*_Unified_${VERSION}_*_Lin64.bin
-	mv ${TOPDIR}/${DOWNLOADDIR}/*_Unified_${VERSION}_*_Lin64.bin "${TOPDIR}/${DOCKERDIR}/build_context/"
+	INSTALLER_BIN=$(find "${TOPDIR}/${DOWNLOADDIR}" -name "FPGAs_AdaptiveSoCs_Unified_${VERSION}_*_Lin64.bin" | head -n 1)
+	if [ -z "${INSTALLER_BIN}" ]; then
+		die "No FPGAs_AdaptiveSoCs_Unified_${VERSION}_*_Lin64.bin file found in '${TOPDIR}/${DOWNLOADDIR}'"
+	fi
+	chmod a+x ${INSTALLER_BIN}
 
 	cd "$DOCKERDIR"
+
 	docker build \
-		--tag ${IMAGE}:${DATE} \
-		--build-arg UID=${UID} \
-		--build-arg GID=${GID} \
+		--tag ${IMAGE} \
+		--build-context installer="${TOPDIR}/${DOWNLOADDIR}" \
+		--build-arg DOXXXER_UID=${DOXXXER_UID} \
+		--build-arg DOXXXER_GID=${DOXXXER_GID} \
 		--build-arg USER=${USER} \
 		--build-arg XILINXMAIL=${XILINXMAIL} \
 		--build-arg XILINXLOGIN=${XILINXLOGIN} \
-		./build_context
-	cd "${TOPDIR}/${DOCKERDIR}"
+		./
+	cd "${TOPDIR}"
 
 else
-	## container around, start
 	cd "${DOCKERDIR}"
 	APP="/bin/bash"
 	if [ ! -e .env ]; then
 		APP=""
-		echo "UID=$(id -u)" > .env
-		echo "GID=$(id -g)" >> .env
-		echo
-		echo "Preparing docker images - please re-run this script to enter the container image!"
+		echo "DOXXXER_UID=$(id -u)" > .env
+		echo "DOXXXER_GID=$(id -g)" >> .env
 	fi
 
 	docker run \
 		--rm \
 		--net host \
 		--name ${IMAGE} \
-		-u ${UID}:${GID} \
+		-u ${DOXXXER_UID}:${DOXXXER_GID} \
 		-it \
 		--privileged \
 		-e USER \
 		-e DISPLAY=$DISPLAY \
 		--env-file .env \
 		--group-add 20 \
-		--mount type=bind,source=./build_configs,target=/home/$USER/configs \
 		-v /tmp/.X11-unix:/tmp/.X11-unix \
 		-v ~/.Xauthority:/home/${USER}/.Xauthority:ro \
 		-v ~/.gitconfig:/home/${USER}/.gitconfig:ro \
-		-v ~/.ssh:/home/${USER}/.ssh \
+		-v ~/.ssh:/home/${USER}/.ssh:ro \
+		-v ./configs:/tmp/host_configs:ro \
 		-v ./workspace:/home/${USER}/workspace \
-		${CONTAINER} \
+		${IMAGE} \
 		${APP}
 
 	exit 0
